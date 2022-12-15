3142a1f1e9b529ba68a05124e462b9216f14d27d
kyle jenne <strongagent1@gmail.com>
2022-12-15 11:16:33 -0500

    exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what is your name?");
+            
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite type of music?");
+            var favMusic = Console.ReadLine();
+
+            Console.WriteLine("What is the best city you have ever visited?");
+            var favCity = Console.ReadLine();
+
+            Console.WriteLine("If you could be an animal what animal would you be?");
+            var favAnimal = Console.ReadLine();
+
+            Console.WriteLine($"Why hello {userName}, welcome to our profile builder.  This can help others learn more about you.  " +
+                $"It appears, based on your answers....that you would love nothing more than to be dressed as a {favAnimal} while riding to " +
+                $"{favCity} to catch your desired {favMusic} concert.  Please use this information wisely {userName}!");
         }
     }
 }
