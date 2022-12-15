d6495030c397143b58d6bcffcd2f8e2206911efe
kyle jenne <strongagent1@gmail.com>
2022-12-15 13:29:48 -0500

    added user choice for math prob type and switch

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Net.WebSockets;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 
@@ -9,38 +11,73 @@ namespace MethodsExercise
         
         public static int Multiply(int num1, int num2)
         {
-            return num1 * num2;
+           var result = num1 * num2;
+            return result;
         }
         public static int Adding(int num1, int num2)
         {
-            return num1 + num2;
+            var result = num1 + num2;
+            return result;
+        }
+        
+        public static int Dividing(int num1, int num2)
+        {
+            var result = num1 / num2;
+            return result;
+        }
+
+        public static int Subtraction(int num1, int num2)
+        {
+            var result = num1 - num2;
+            return result;
         }
 
 
         static void  Main(string[] args)
         {
+            Console.WriteLine("Please tell me what type of Math problem you would like to solve based on the corresponding number below.");
+            Console.WriteLine("'1' = Adding");
+            Console.WriteLine("'2' = Subtraction");
+            Console.WriteLine("'3' = Multiply");
+            Console.WriteLine("'4' = Division");
+
+            int mathType = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please tell me the first number  you would like to use.");
+            var num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please tell me the second number  you would like to use.");
+            var num2 = Convert.ToInt32(Console.ReadLine());
 
             
-                Console.WriteLine("please tell me the first number  you would like to add.");
-                var num1Add = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("please tell me the second number  you would like to add.");
-                var num2Add = int.Parse(Console.ReadLine());
-
-
-                Console.WriteLine($"The sum is {Adding(num1Add, num2Add)}.");
-
-
-
-
-                Console.WriteLine("please tell me the first number  you would like to multiply.");
-                var num1Multiply = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("please tell me the second number  you would like to multiply.");
-                var num2Multiply = int.Parse(Console.ReadLine());
+            switch (mathType)
+            {
+                case 1: Console.WriteLine($"Addition value is : {Adding(num1, num2)}");
+                    break;
+                case 2: Console.WriteLine($"Subtraction value is : {Subtraction(num1, num2)}");
+                    break;
+                case 3: Console.WriteLine($"Multiplication value is : {Multiply(num1, num2)}");
+                    break;
+                case 4: Console.WriteLine($"Division value is : {Dividing(num1, num2)}");
+                    break;
 
 
-                Console.WriteLine($"The product is {Multiply(num1Multiply, num2Multiply)}.");
+            }
+
+            
+
+
+
+
+                //Console.WriteLine("please tell me the first number  you would like to multiply.");
+                //var num1Multiply = int.Parse(Console.ReadLine());
+
+                //Console.WriteLine("please tell me the second number  you would like to multiply.");
+                //var num2Multiply = int.Parse(Console.ReadLine());
+
+
+                //Console.WriteLine($"The product is {Multiply(num1Multiply, num2Multiply)}.");
 
 
 
